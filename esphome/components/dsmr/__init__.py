2bbfec4d180a468671fdf6efb1eeb1b927c5d42b
Maarten Hostyn <mhostyn@users.noreply.github.com>
2023-02-10 19:20:18 +0100

    Update __init__.py
    Use latest version of library

--- esphome/components/dsmr/__init__.py
+++ esphome/components/dsmr/__init__.py
@@ -84,7 +84,10 @@ async def to_code(config):
     cg.add_build_flag("-DDSMR_GAS_MBUS_ID=" + str(config[CONF_GAS_MBUS_ID]))
 
     # DSMR Parser
-    cg.add_library("glmnet/Dsmr", "0.5")
-
+    cg.add_library(
+        name="Dsmr",
+        repository="https://github.com/glmnet/arduino-dsmr",
+        version="master",
+    )
     # Crypto
     cg.add_library("rweather/Crypto", "0.4.0")
